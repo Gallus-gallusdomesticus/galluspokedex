8bd20da9f6e06d425aff9377062ec97b3b24f5e2
Gallus-gallusdomesticus <stephenefelson1999@gmail.com>
2026-01-03 14:41:14 +1100

    move the replbase function from main.go to repl.go, modify gitignore

--- main.go
+++ main.go
@@ -1,19 +1,5 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-)
-
 func main() {
-	newscan := bufio.NewScanner(os.Stdin)
-
-	for {
-		fmt.Print("Pokedex > ")
-		newscan.Scan()
-		input := newscan.Text()
-		cleaned := cleanInput(input)
-		fmt.Println("Your command was:", cleaned[0])
-	}
+	replbase()
 }
--- repl.go
+++ repl.go
@@ -1,12 +1,29 @@
 package main
 
-import("strings")
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func replbase() {
+	newscan := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex > ")
+		newscan.Scan()
+		input := newscan.Text()
+		cleaned := cleanInput(input)
+		fmt.Println("Your command was:", cleaned[0])
+	}
+}
 
 func cleanInput(text string) []string {
-    lowered:=strings.ToLower(text) //lower all uppercase
+	lowered := strings.ToLower(text)           //lower all uppercase
 	nowhitespace := strings.TrimSpace(lowered) //remove all trailing whitespace
 
 	separated := strings.Fields(nowhitespace) //separate into slice
-	
+
 	return separated
 }
